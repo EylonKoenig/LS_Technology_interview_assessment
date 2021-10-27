1b299005be6f7cbf664f30713af3b32f304f0455
Eylon Koenig <eylonk@printsyst.ai>
2021-10-27 22:26:24 +0300

    remove credentials

--- frontend/src/common/api.js
+++ frontend/src/common/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-
 const api = axios.create({
   baseURL: "http://127.0.0.1:5000/",
   headers: {
     "Content-Type": "application/json",
     "X-CSRFToken": null,
+    // crossorigin: true,
   },
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default api;
