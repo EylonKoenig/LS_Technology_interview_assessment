43fb4f17edbd1ea9be4a453d3f32e160608a9bcf
Eylon Koenig <eylonk@printsyst.ai>
2021-10-27 22:29:12 +0300

    auth api

--- frontend/src/common/auth.js
+++ frontend/src/common/auth.js
@@ -1,6 +1,26 @@
 import api from "../common/api";
 
-export const loadUser = async (dispatch) => {
+export const loginUser = async (inputData, dispatch) => {
   try {
+    const response = await api.post("/login", inputData);
+    const { access_token } = response.data;
+    setAuthToken(access_token);
+    if (response.status === 200) {
+      dispatch({
+        type: "USER_LOADED",
+      });
+    }
+    return response.status;
   } catch (err) {}
+};
+
+export const loadUser = async (inputData, dispatch) => {
+  try {
+    const response = await api.post("/login", inputData);
+    console.log(response);
+  } catch (err) {}
+};
+export const setAuthToken = (token) => {
+  api.defaults.headers.common["Authorization"] = "Bearer " + token;
+  localStorage.setItem("access_token", token);
 };
