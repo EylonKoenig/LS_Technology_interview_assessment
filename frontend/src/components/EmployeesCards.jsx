8d9032515d7f5ab47fc23601e07b05928a05a0bc
Eylon Koenig <eylonk@printsyst.ai>
2021-10-30 21:09:53 +0300

    initial cards

--- frontend/src/components/EmployeesCards.jsx
+++ frontend/src/components/EmployeesCards.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { ReactComponent as EllipsisIcon } from "../styles/svg/ellipsis.svg";
+import "../styles/employeesCard.css";
 
 const EmployeesCards = ({ employees }) => {
-  return <div></div>;
+  return (
+    <div className="cards-wrapper">
+      <div className="cards-headers">
+        <h3>Managing Employees</h3>
+        <span class="circle plus"></span>
+      </div>
+      <div className="cards">
+        {employees.map((employee, index) => (
+          <div className="card">
+            <img src="/svg/login-face.png" alt="logo" height="40" width="40" />
+            <div>
+              <div>
+                <span>{employee.firstName}</span>
+                <span>{employee.lastName}</span>
+                <div />
+                <div>{employee.roll}</div>
+                <div>{employee.phone}</div>
+                <div>{employee.address}</div>
+              </div>
+            </div>
+            <EllipsisIcon style={{ height: "15px" }} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default EmployeesCards;
