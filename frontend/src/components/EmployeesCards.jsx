34360e61a6f8ec98adae8fdef61dab63e85005fa
Eylon Koenig <eylonk@printsyst.ai>
2021-10-31 14:13:34 +0200

    card style

--- frontend/src/components/EmployeesCards.jsx
+++ frontend/src/components/EmployeesCards.jsx
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as EllipsisIcon } from "../styles/svg/ellipsis.svg";
 import { ReactComponent as PhoneIcon } from "../styles/svg/phone.svg";
 import { ReactComponent as FolderIcon } from "../styles/svg/folder.svg";
 import { ReactComponent as LocationIcon } from "../styles/svg/location.svg";
+import { ReactComponent as PenIcon } from "../styles/svg/pen.svg";
 import "../styles/employeesCard.css";
+import AddEmployeeModal from "../modal/AddEmployeeModal";
+import CardFunctionalityModal from "../modal/CardFunctionalityModal";
 
 const EmployeesCards = ({ employees }) => {
+  const [showAddEmpModal, setShowAddEmpModal] = useState(false);
+  const [showFuncModal, setShowFuncModal] = useState(false);
+  const [userToEdit, setUserToEdit] = useState(null);
+
+  const showEditModal = (employee) => {
+    setUserToEdit(employee);
+    setShowFuncModal(true);
+  };
+
   return (
     <div className="cards-wrapper">
+      {showAddEmpModal ? (
+        <AddEmployeeModal closeModal={() => setShowAddEmpModal(false)} />
+      ) : null}
+      {showFuncModal ? (
+        <CardFunctionalityModal
+          closeModal={() => setShowFuncModal(false)}
+          employee={userToEdit}
+        />
+      ) : null}
       <div className="cards-headers">
         <h3>Managing Employees</h3>
-        <span className="circle plus"></span>
+        <span
+          className="circle plus"
+          onClick={() => setShowAddEmpModal(true)}
+        ></span>
       </div>
       <div className="cards">
         {employees.map((employee, index) => (
           <div className="card" key={index}>
-            <img src="/svg/login-face.png" alt="logo" height="40" width="40" />
+            <img
+              src={`/person_${index + 1}.png`}
+              alt="person"
+              height="40"
+              width="40"
+            />
             <div className="card-details">
               <div>
-                <span>{employee.firstName}</span>
-                <span>{employee.lastName}</span>
-                <div />
+                <div className="employee-name">
+                  <span>{employee.firstName}</span>
+                  <span>{employee.lastName}</span>
+                </div>
                 <div>
                   <FolderIcon />
                   {employee.roll}
+                </div>
+                <div>
+                  <PenIcon />
+                  {employee.created}
                 </div>
                 <div>
                   <PhoneIcon />
@@ -35,7 +69,9 @@ const EmployeesCards = ({ employees }) => {
                 </div>
               </div>
             </div>
-            <EllipsisIcon style={{ height: "15px" }} />
+            <div onClick={() => showEditModal(employee)}>
+              <EllipsisIcon style={{ height: "15px" }} />
+            </div>
           </div>
         ))}
       </div>
