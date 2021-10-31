ba91b17f760062813998383aa2b4e78181526765
Eylon Koenig <eylonk@printsyst.ai>
2021-10-31 09:28:35 +0200

    employee card

--- frontend/src/components/EmployeesCards.jsx
+++ frontend/src/components/EmployeesCards.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { ReactComponent as EllipsisIcon } from "../styles/svg/ellipsis.svg";
+import { ReactComponent as PhoneIcon } from "../styles/svg/phone.svg";
+import { ReactComponent as FolderIcon } from "../styles/svg/folder.svg";
+import { ReactComponent as LocationIcon } from "../styles/svg/location.svg";
 import "../styles/employeesCard.css";
 
 const EmployeesCards = ({ employees }) => {
@@ -11,16 +14,25 @@ const EmployeesCards = ({ employees }) => {
       </div>
       <div className="cards">
         {employees.map((employee, index) => (
-          <div className="card">
+          <div className="card" key={index}>
             <img src="/svg/login-face.png" alt="logo" height="40" width="40" />
-            <div>
+            <div className="card-details">
               <div>
                 <span>{employee.firstName}</span>
                 <span>{employee.lastName}</span>
                 <div />
-                <div>{employee.roll}</div>
-                <div>{employee.phone}</div>
-                <div>{employee.address}</div>
+                <div>
+                  <FolderIcon />
+                  {employee.roll}
+                </div>
+                <div>
+                  <PhoneIcon />
+                  {employee.phone}
+                </div>
+                <div>
+                  <LocationIcon />
+                  {employee.address}
+                </div>
               </div>
             </div>
             <EllipsisIcon style={{ height: "15px" }} />
